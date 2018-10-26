1b595f9005fe6e1a0fdefb1f90542e4daa1aebf9
Wejknis <weja077@gmail.com>
2018-10-26 09:32:16 +0200

    największa, mniejsza od podanej przez użytkonika, jednoszcześnie niepodzielna przez 2, 3, 5, 7

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 int main() {
-int b;
+int b = 0;
 int licznik_petli = 0;
-for (b = 999; !(b%3) || !(b%5) || !(b%7); b-=2){
+
+    printf("Program podaje największą liczbę, mniejszą od\n");
+    printf("podanej przez użytkownika,\n");
+    printf("jednocześnie niepodzielną przez 2, 3, 5 i 7\n\n");
+    printf("Podaj liczbę b: ");
+    scanf("%d", &b);
+    // zrób nieparzystą
+    if(!(b % 2)) --b;
+       for (; !(b % 3) || !(b % 5) || !(b % 7); b -= 2) {
                 licznik_petli++;
         }
     printf("znaleziona liczba: %d\n", b);
