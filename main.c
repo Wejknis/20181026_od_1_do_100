2ecf4453f7b25a9a9816dcb6ffa7c5e3be46daf8
Wejknis <weja077@gmail.com>
2018-10-26 09:47:44 +0200

    największa liczba, mniejsza od podanej przez użytkownika, jednocześnie niepodzielna przez 2, 3 ,5 ,7 z pętlą for i break

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main() {
 int b = 0;
+int i = 0;
 int licznik_petli = 0;
 
     printf("Program podaje największą liczbę, mniejszą od\n");
@@ -10,11 +11,14 @@ int licznik_petli = 0;
     printf("Podaj liczbę b: ");
     scanf("%d", &b);
     // zrób nieparzystą
-    if(!(b % 2)) --b;
-       for (; !(b % 3) || !(b % 5) || !(b % 7); b -= 2) {
+            for (i = b; i > 0; --i) {
                 licznik_petli++;
+            if (i%2 && i%3 && i%5 && i%7){
+                printf("%d", i);
+                break;
+            }
         }
-    printf("znaleziona liczba: %d\n", b);
+    printf("znaleziona liczba: %d\n", i);
 
     printf("\n");
     printf("licznik pętli wynosiła: %d\n", licznik_petli);
