d144072b0236112fdecb68d03d9304f20daf22f2
Wejknis <weja077@gmail.com>
2018-10-26 08:31:14 +0200

    aaa od 100 do 1 tylko parzyste ver.2

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 int main() {
 int b;
-    for (b = 100; b >= 1 ; --b)
+int licznik_petli = 0;
+    for (b = 100; b >= 1 ; b -=2)
     {
-        if (!(b%2 ))
-        {
-            printf("%d\t ", b);
+    printf("%d\t ", b);
+
+            licznik_petli++;
         }
-    }
+    printf("\n");
+    printf("licznik pętli wynosiła: %d\n", licznik_petli);
     return 0;
 }
