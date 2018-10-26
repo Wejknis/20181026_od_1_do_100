d50ab7b534811d4075ffb36fa405224d6779d823
Wejknis <weja077@gmail.com>
2018-10-26 08:57:07 +0200

    aaa największa liczba, mniejsza od 2000 i niepodzielna przez 2, 3, 5, 7

--- main.c
+++ main.c
@@ -3,13 +3,12 @@
 int main() {
 int b;
 int licznik_petli = 0;
-    for (b = 100; b >= 1 ; b -=2)
-    {
-    printf("%d\t ", b);
-
-            licznik_petli++;
+for (b = 1999; !(b%3) || !(b%5) || !(b%7); b-=2){
+                licznik_petli++;
         }
+    printf("znaleziona liczba: %d\n", b);
+
     printf("\n");
     printf("licznik pętli wynosiła: %d\n", licznik_petli);
-    return 0;
-}
+        return 0;
+    }
